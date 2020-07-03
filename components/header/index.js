640334a2e43bea2193093630237b20bd319ee331
ereio <github@ere.io>
2020-07-03 15:25:41 -0400

    clean up

--- components/header/index.js
+++ components/header/index.js
@@ -3,12 +3,14 @@ import Link from 'next/link'
 import React, { Component } from 'react';
 
 import assets from '../../global/assets';
+import styles from './header.module.css'
+
 
 export default class Header extends Component {
     render() {
         return (
-            <nav id="header" class="fixed w-full z-30 top-0 gradient">
-                <div class="container flex flex-wrap items-center justify-between w-full mx-auto mt-0 px-2 py-4">
+            <nav id="header" className="fixed w-full z-30 top-0 header">
+                <div class="container flex flex-wrap items-center justify-between mx-auto mt-0 px-2 py-4">
                     <a className="flex items-center flex-shrink-0 text-white">
                         <div className="fill-current h-12 w-12">
                             <img src={assets.iconAppWhite} />
@@ -17,19 +19,23 @@ export default class Header extends Component {
                             syphon
                         </span>
                     </a>
-                    <div className="block lg:hidden">
-                        <button className="flex items-center px-3 py-2 border rounded text-white border-teal-400 hover:text-white hover:border-white">
-                            <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+                    <div class="flex items-center justify-end w-full md:hidden">
+                        <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+                            <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
                         </button>
                     </div>
-                    <div className="flex items-center w-full lg:w-auto">
-                        <div className="flex-grow items-center content-center text-sm">
-                            <a className="text-base text-teal-200 hover:text-white mr-4" href="https://github.com/syphon-org/syphon/blob/main/readme.md">About</a>
-                            <a className="text-base text-teal-200 hover:text-white mr-4" href="/support">Support</a>
-                        </div>
-                        <div>
-                            <a className="border rounded text-white border-white text-sm mt-4 px-4 py-2 hover:border-transparent hover:text-teal-500 hover:bg-white" href="https://github.com/syphon-org/syphon/releases">Download</a>
-
+                    <div class="w-full justify-end flex flex-wrap md:items-center md:w-auto">
+                        <div class="text-sm text-right items-center md:flex-grow">
+                            <a href="#responsive-header" class="block  mt-4 md:mt-0 md:mr-4 text-teal-200 hover:text-white md:inline-block  ">
+                                About
+                            </a>
+                            <a href="/support" class="block  mt-4 md:mt-0 md:mr-4 text-teal-200 hover:text-white md:inline-block ">
+                                Support
+                            </a>
+                            <a href="https://github.com/syphon-org/syphon/releases"
+                                class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">
+                                Download
+                            </a>
                         </div>
                     </div>
                 </div>
--- pages/support/index.js
+++ pages/support/index.js
@@ -26,23 +26,4 @@ function Support() {
     )
 }
 
-
-
-export async function getStaticProps() {
-    // Call an external API endpoint to get posts
-    // const res = await fetch('https://.../posts')
-    // const posts = await res.json()
-
-    // const things = thingsJson.map((json) => JSON.parse(json));
-
-    // By returning { props: posts }, the Blog component
-    // will receive `posts` as a prop at build time
-    return {
-        props: {
-            things: {},
-        },
-    }
-}
-
-
 export default Support
