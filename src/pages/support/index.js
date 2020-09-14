18adf5596233604d1b3eb0435a40217fe2999618
taylor <github@ere.io>
2020-09-13 21:42:28 -0400

    [Meta] Branding Updates (#3)
    * website updates for version 0.1.3 branding changes
    
    * static build output
    
    * new branding and website tweaks

--- src/pages/support/index.js
+++ src/pages/support/index.js
@@ -4,26 +4,26 @@ import Link from 'next/link'
 import assets from '../../global/assets';
 
 function Support() {
-    return (
-        <div className="app sans-serif">
-            <div class="splash flex flex-grow flex-col justify-center items-center">
-                <div class="flex-col justify-center items-center text-center">
-                    <div class="flex-row justify-center items-center text-center">
-                        <a href="/home">
-                            <img class="m-auto" style={{ height: 140, }} src={assets.iconAppWhite} />
-                        </a>
-                    </div>
-                    <div class="pt-8">
-                        <span class="text-4xl">
-                            need help?
+  return (
+    <div className="app sans-serif">
+      <div class="splash flex flex-grow flex-col justify-center items-center">
+        <div class="flex-col justify-center items-center text-center">
+          <div class="flex-row justify-center items-center text-center">
+            <a href="/home">
+              <img class="m-auto" style={{ height: 140, }} src={assets.appIconWhite} />
+            </a>
+          </div>
+          <div class="pt-8">
+            <span class="text-4xl">
+              need help?
                         </span>
-                        <a href='mailto:support@syphon.org?subject=Syphon%20App%20Help&body=Leave%20your%20feedback%20or%20question%20here' class="text-4xl mx-2" style={{ cursor: 'pointer', letterSpacing: '0.5', textDecorationLine: 'underline' }}>email us</a>
-                        <div>at support@syphon.org</div>
-                    </div>
-                </div>
-            </div>
-        </div >
-    )
+            <a href='mailto:support@syphon.org?subject=Syphon%20App%20Help&body=Leave%20your%20feedback%20or%20question%20here' class="text-4xl mx-2" style={{ cursor: 'pointer', letterSpacing: '0.5', textDecorationLine: 'underline' }}>email us</a>
+            <div>at support@syphon.org</div>
+          </div>
+        </div>
+      </div>
+    </div >
+  )
 }
 
 export default Support
--- src/pages/under/index.js
+++ src/pages/under/index.js
@@ -5,41 +5,41 @@ import Head from 'next/head'
 import assets from '../../global/assets';
 
 function Main({ props }) {
-    return (
-        <div className="app sans-serif">
-            <Head>
-                <title>Syphon • A privacy centric matrix client</title>
-            </Head>
-            <div class="splash flex flex-grow flex-col justify-center items-center">
-                <div class="flex-col justify-center items-center text-center">
-                    <div class="flex-row justify-center items-center text-center">
-                        <img class="m-auto" style={{ height: 140, }} src={assets.iconAppWhite} />
-                    </div>
-                    <div class="pt-8">
-                        <span class="text-4xl">almost here</span>
-                    </div>
-                </div>
-            </div>
-        </div >
-    )
+  return (
+    <div className="app sans-serif">
+      <Head>
+        <title>Syphon • A privacy centric matrix client</title>
+      </Head>
+      <div class="splash flex flex-grow flex-col justify-center items-center">
+        <div class="flex-col justify-center items-center text-center">
+          <div class="flex-row justify-center items-center text-center">
+            <img class="m-auto" style={{ height: 140, }} src={assets.appIconWhite} />
+          </div>
+          <div class="pt-8">
+            <span class="text-4xl">almost here</span>
+          </div>
+        </div>
+      </div>
+    </div >
+  )
 }
 
 
 
 export async function getStaticProps() {
-    // Call an external API endpoint to get posts
-    // const res = await fetch('https://.../posts')
-    // const posts = await res.json()
+  // Call an external API endpoint to get posts
+  // const res = await fetch('https://.../posts')
+  // const posts = await res.json()
 
-    // const things = thingsJson.map((json) => JSON.parse(json));
+  // const things = thingsJson.map((json) => JSON.parse(json));
 
-    // By returning { props: posts }, the Blog component
-    // will receive `posts` as a prop at build time
-    return {
-        props: {
-            things: {},
-        },
-    }
+  // By returning { props: posts }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      things: {},
+    },
+  }
 }
 
 
