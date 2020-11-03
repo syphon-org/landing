0923bd1fe27c1b29d2ae2a91b4b3e29c13e89669
ereio <git@ere.io>
2020-11-02 23:31:31 -0500

    adding cdn assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,2 +1,5 @@
 const withImages = require('next-images')
-module.exports = withImages()
+module.exports = withImages({
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? 'https://restless-heart-7405.on.fleek.co' : '',
+})
