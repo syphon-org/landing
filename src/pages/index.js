e826c3d468e5c8a8eb63045dfe5844a1112866c8
ereio <git@ere.io>
2020-11-23 20:52:29 -0500

    typo and devDependency security dep upgrade

--- src/pages/index.js
+++ src/pages/index.js
@@ -97,7 +97,7 @@ function Main() {
               <div className="align-middle">
                 <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">end to end encrypted</h3>
                 <p className="text-gray-600 mb-8">
-                  Syphon uses the olm/megolm encryption library provided by Matrix to keep your chats private from others
+                  Using the open-source Olm/Megolm encryption library provided by Matrix, your chats private from others
                 </p>
               </div>
             </div>
