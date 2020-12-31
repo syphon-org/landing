9ab8934b6a6abe11221b7369e217449353ed8cdf
ereio <git@ere.io>
2020-12-31 16:09:15 -0500

    dont add js to out dir

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,36 @@
 module.exports = {
-    env: {
-        browser: true,
-        es6: true,
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-        'plugin:react/recommended',
-        'airbnb',
-    ],
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
-    },
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 11,
-        sourceType: 'module',
-    },
-    plugins: [
-        'react',
-    ],
-    rules: {
-        'react/prop-types': 0,
-        "react/react-in-jsx-scope": 0,
-        'import/prefer-default-export': 0,
-        'react/prefer-stateless-function': [0, { ignorePureComponents: true }],
-        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-        "react/jsx-max-props-per-line": [2, { "maximum": 2, "when": "multiline" }],
-        'react/destructuring-assignment': [0, 'always', { ignoreClassFields: true }],
-    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 0,
+    'import/prefer-default-export': 0,
+    'react/prefer-stateless-function': [0, { ignorePureComponents: true }],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-max-props-per-line': [2, { maximum: 2, when: 'multiline' }],
+    'react/destructuring-assignment': [0, 'always', { ignoreClassFields: true }],
+    'max-classes-per-file': 0,
+    'no-underscore-dangle': 0,
+    'react/jsx-props-no-spreading': 0,
+  },
 };
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,46 +1,62 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, {
+  Html, Head, Main, NextScript,
+} from 'next/document';
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
-
+class HeadProduction extends Head {
   render() {
+    const { head } = this.context._documentProps;
+    const { children } = this.props;
     return (
-      <Html>
-        <Head>
-          <link
-            rel="preload"
-            href="/fonts/Rubik-Regular.ttf"
-            as="Rubik"
-            crossOrigin=""
-          />
-          <link
-            rel="preload"
-            href="/fonts/Rubik-Light.ttf"
-            as="Rubik-Light"
-            crossOrigin=""
-          />
-          <link
-            rel="preload"
-            href="/fonts/Rubik-Bold.ttf"
-            as="Rubik-Bold"
-            crossOrigin=""
-          />
-          <link rel="shortcut icon" href="favicon.ico" />
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-          <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta property="og:image" content="app-logo.png" />
-        </Head>
-        <body class="leading-normal tracking-normal text-white gradient">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
+      <head {...this.props}>
+        {children}
+        {head}
+        <link
+          rel="preload"
+          href="/fonts/Rubik-Regular.ttf"
+          as="Rubik"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/Rubik-Light.ttf"
+          as="Rubik-Light"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/Rubik-Bold.ttf"
+          as="Rubik-Bold"
+          crossOrigin=""
+        />
+        <link rel="shortcut icon" href="favicon.ico" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta property="og:image" content="app-logo.png" />
+        {this.getCssLinks()}
+      </head>
+    );
   }
 }
 
-export default MyDocument
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    const isDev = process.env.NODE_ENV === 'development';
+    return (
+      <Html>
+        {isDev ? <Head /> : <HeadProduction />}
+        <body className="leading-normal tracking-normal text-white gradient">
+          <Main />
+          {isDev && <NextScript />}
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
