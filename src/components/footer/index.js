220858a4a07419d9dbbe5212347c2df8522fdffa
ereio <git@ere.io>
2021-01-29 23:53:02 -0500

    updating copywrite

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 import React, { Component } from 'react';
 
@@ -7,75 +7,74 @@ import assets from '../../global/assets';
 export default class Footer extends Component {
   render() {
     return (
-      <footer class="bg-brand">
-        <div class="container mx-auto  px-8">
+      <footer className="bg-brand">
+        <div className="container mx-auto  px-8">
 
-          <div class="w-full flex flex-col md:flex-row py-6">
+          <div className="w-full flex flex-col md:flex-row py-6">
 
-            <div class="flex-1 justify-content mb-6">
-              <a class="text-orange-600 no-underline hover:no-underline font-bold text-2xl lg:text-4xl flex flex-row" href="#">
-                <img src={assets.appLogoWhite} class="h-10 fill-current inline" />
-                <span class={"text-white text-sm mb-8 pl-1"}>®</span>
+            <div className="flex-1 justify-content mb-6">
+              <a className="text-orange-600 no-underline hover:no-underline font-bold text-2xl lg:text-4xl flex flex-row" href="#">
+                <img src={assets.appLogoWhite} className="h-10 fill-current inline" />
+                <span className="text-white text-sm mb-8 pl-1">®</span>
               </a>
             </div>
 
-
-            <div class="flex-1">
-              <p class="uppercase text-white-500 md:mb-6">Links</p>
-              <ul class="list-reset mb-6">
-                <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                  <a href="/under" class="no-underline hover:underline text-white-800 hover:text-orange-500">faq</a>
+            <div className="flex-1">
+              <p className="uppercase text-white-500 md:mb-6">Links</p>
+              <ul className="list-reset mb-6">
+                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                  <a href="/under" className="no-underline hover:underline text-white-800 hover:text-orange-500">faq</a>
                 </li>
-                <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                  <a href="mailto:contact@syphon.org?subject=Syphon%20App&body" class="no-underline hover:underline text-white-800 hover:text-orange-500">contact</a>
+                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                  <a href="mailto:contact@syphon.org?subject=Syphon%20App&body" className="no-underline hover:underline text-white-800 hover:text-orange-500">contact</a>
                 </li>
-                <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                  <a href="/support" class="no-underline hover:underline text-white-800 hover:text-orange-500">support</a>
-                </li>
-              </ul>
-            </div>
-            <div class="flex-1">
-              <p class="uppercase text-white-500 md:mb-6">Legal</p>
-              <ul class="list-reset mb-6">
-                <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                  <a href="/terms" class="no-underline hover:underline text-white-800 hover:text-orange-500">terms</a>
-                </li>
-                <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                  <a href="/privacy" class="no-underline hover:underline text-white-800 hover:text-orange-500">privacy</a>
+                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                  <a href="/support" className="no-underline hover:underline text-white-800 hover:text-orange-500">support</a>
                 </li>
               </ul>
             </div>
-            <div class="flex-1">
-              <p class="uppercase text-white-500 md:mb-6">Social</p>
-              <ul class="list-reset mb-6">
-                <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                  <a href="https://www.instagram.com/syphonapp" class="no-underline hover:underline text-white-800 hover:text-orange-500">instagram</a>
+            <div className="flex-1">
+              <p className="uppercase text-white-500 md:mb-6">Legal</p>
+              <ul className="list-reset mb-6">
+                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                  <a href="/terms" className="no-underline hover:underline text-white-800 hover:text-orange-500">terms</a>
                 </li>
-                <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                  <a href="https://github.com/syphon-org" class="no-underline hover:underline text-white-800 hover:text-orange-500">github</a>
-                </li>
-                <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                  <a href="https://twitter.com/syphonapp" class="no-underline hover:underline text-white-800 hover:text-orange-500">twitter</a>
+                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                  <a href="/privacy" className="no-underline hover:underline text-white-800 hover:text-orange-500">privacy</a>
                 </li>
               </ul>
             </div>
-            <div class="flex-1">
-              <p class="uppercase text-white-500 md:mb-6">more</p>
-              <ul class="list-reset mb-6">
-                <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                  <a href="/about" class="no-underline hover:underline text-white-800 hover:text-orange-500">about us</a>
+            <div className="flex-1">
+              <p className="uppercase text-white-500 md:mb-6">Social</p>
+              <ul className="list-reset mb-6">
+                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                  <a href="https://www.instagram.com/syphonapp" className="no-underline hover:underline text-white-800 hover:text-orange-500">instagram</a>
+                </li>
+                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                  <a href="https://github.com/syphon-org" className="no-underline hover:underline text-white-800 hover:text-orange-500">github</a>
+                </li>
+                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                  <a href="https://twitter.com/syphonapp" className="no-underline hover:underline text-white-800 hover:text-orange-500">twitter</a>
+                </li>
+              </ul>
+            </div>
+            <div className="flex-1">
+              <p className="uppercase text-white-500 md:mb-6">more</p>
+              <ul className="list-reset mb-6">
+                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                  <a href="/about" className="no-underline hover:underline text-white-800 hover:text-orange-500">about us</a>
                 </li>
               </ul>
             </div>
           </div>
         </div>
 
-        <div class="credits justify-center flex flex-col items-center" style={{ paddingBottom: 16, }}>
-          <span class="text-white">Copyright © 2020 Ereio, LLC</span>
-          <span class="text-white">Syphon ® is a registered trademark</span>
+        <div className="credits justify-center flex flex-col items-center" style={{ paddingBottom: 16 }}>
+          <span className="text-white">Copyright © 2021 Ereio, LLC</span>
+          <span className="text-white">Syphon ® is a registered trademark</span>
         </div>
-        <div class="credits justify-center flex" />
+        <div className="credits justify-center flex" />
       </footer>
-    )
+    );
   }
 }
