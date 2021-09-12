a9148d9717bd6375a760baf8ebc6058f77834159
Zipperflunky <theopaspam@gmail.com>
2021-09-12 22:09:44 +0200

    changed matrix to faq

---
+++ src/pages/faq/index.js
@@ -0,0 +1,33 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable max-len */
+import React from 'react'
+
+import Head from 'next/head'
+
+import { Header } from '../../components/header'
+
+export const config = {
+  unstable_runtimeJS: false
+}
+
+export default function About() {
+  return (
+    <div className="app sans-serif bg-white">
+      <Head>
+        <title>Syphon • Matrix</title>
+      </Head>
+      <Header />
+      <section className="pt-40 pb-24 px-4 bg-white">
+        <div className="container mx-auto flex flex-wrap flex-col md:flex-row justify-between items-center">
+          <section className="container text-black max-w-5xl mx-auto">
+            <div className="flex flex-col justify-center w-4/6">
+              <h4 className="text-6xl text-teal mb-6 rubik">FAQ</h4>
+              <p className="text-xl text-black">
+              </p>
+            </div>
+          </section>
+        </div>
+      </section>
+    </div>
+  )
+}
